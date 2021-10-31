877f3996d5eae9045ff7e1a7c0df52a80f526815
Elistratishka <elistratishka@outlook.com>
2021-10-31 12:57:41 +0300

    Update Telegram_Bot.py

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -1,5 +1,5 @@
 import os
-import  telebot
+import telebot
 from dotenv import load_dotenv
 from extensions import get_data, convert, ConvertionException
 
@@ -8,27 +8,27 @@ token = os.getenv("TOKEN")
 bot = telebot.TeleBot(token)
 
 
-
 @bot.message_handler(commands=['start', 'help'])
-def help(message: telebot.types.Message):
-    rules = "Бот применим для конвертации стоимости инстранных валют и рубля по курсу ЦБ на сегодня, "\
-            "а также для конвертации стоимости токенов с площадки alcor exchange\n"\
+def help_(message: telebot.types.Message):
+    rules = "Бот применим для конвертации стоимости инстранных валют и рубля по курсу ЦБ на сегодня, " \
+            "а также для конвертации стоимости токенов с площадки alcor exchange\n" \
             "Для использования бота напишите ему сообщение в формате:\n <количество валюты>" \
             " <валюта из которой переводить> <валюта в которую переводить>\n" \
             "Введите команду /values для получения информации о доступных для конвертации валютах и токенах"
     bot.reply_to(message, rules)
+
 
 @bot.message_handler(commands=['values'])
 def start(message: telebot.types.Message):
     text = 'Доступные валюты: '
     data = get_data()
     for values in data.keys():
-        text = '\n'.join((text, values, ))
+        text = '\n'.join((text, values,))
     bot.reply_to(message, text)
+
 
 @bot.message_handler(content_types=['text'])
 def work(message: telebot.types.Message):
-    data = get_data()
     try:
         letter = message.text.replace(',', '.').upper().split(' ')
         if len(letter) != 3:
@@ -38,14 +38,13 @@ def work(message: telebot.types.Message):
     except ConvertionException as e:
         bot.reply_to(message, f'Ошибка: {e}')
     except Exception:
-        bot.send_message('Извините, что-то пошло не так')
+        bot.send_message(message.chat.id, 'Извините что-то пошло не так')
     else:
         text = f'Стоимость {amount} {quote} в {base} = {cost}'
         bot.send_message(message.chat.id, text)
 
 
 bot.infinity_polling()
-
 
 
 
